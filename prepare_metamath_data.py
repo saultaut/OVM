95685e422da64301e9097440f628c8a5c71d5d13
Saulius <saulius.tautvaisas@gmail.com>
2024-02-26 06:45:29 +0200

    prepare the datasize

--- prepare_metamath_data.py
+++ prepare_metamath_data.py
@@ -1,12 +1,21 @@
 from datasets import load_dataset
+import argparse
 import json
 import io
 
 
-
+# Create the parser
+parser = argparse.ArgumentParser(description="Dataset size.")
+# Define the --data_size argument
+parser.add_argument('--data_size', type=int, default=10, help='The size of the data')
+# Parse the arguments
+args = parser.parse_args()
+# Now you can use args.data_size in your script
+print(f"Data size is set to: {args.data_size}")
+      
 dataset = load_dataset("meta-math/MetaMathQA")
 
-n = 1000 #number of datapoints
+n = args.data_size #number of datapoints
 
 # Select a subset of the dataset
 subset = dataset['train'].select(range(n))
